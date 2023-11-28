47299871ee3d734d31b44318bbeb6d052767db73
mizore-fu <mhxxname0782@gmail.com>
2023-11-28 15:08:51 +0900

    アプリの大まかなUIを作成

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Form } from "./components/Form";
+import { Todo } from "./components/Todo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>TODO List</h1>
+      <Form />
+      <div>
+        <Todo />
+        <Todo />
+        <Todo />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ app/src/components/Todo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Todo = () => {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Task</p>
+      <button>delete</button>
+    </div>
+  );
+};
