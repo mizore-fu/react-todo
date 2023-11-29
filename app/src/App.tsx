2f682b8a59d82cd684933fc42b31bd8d187c18fa
mizore-fu <mhxxname0782@gmail.com>
2023-11-29 15:52:52 +0900

    タスクの追加機能を作成

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { ulid } from "ulid";
 import { Form } from "./components/Form";
 import { Todo } from "./components/Todo";
 import { Task } from "./types";
 
-const tasks: Task[] = [
-  { id: "1", name: "walk", completed: false },
-  { id: "2", name: "sleep", completed: false },
-  { id: "3", name: "sleep", completed: false },
-];
-
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: "taskid-1", name: "walk", completed: false },
+    { id: "taskid-2", name: "sleep", completed: false },
+    { id: "taskid-3", name: "eat", completed: false },
+  ]);
+
+  const addTask = (name: string) => {
+    const newTask: Task = {
+      id: `taskid-${ulid()}`,
+      name: name,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className="App">
       <h1>TODO List</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div>
         {tasks.map((task: Task) => {
           return (
--- app/src/components/Form.tsx
+++ app/src/components/Form.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { FC, useState } from "react";
 
-export const Form = () => {
+type Props = {
+  addTask: (name: string) => void;
+};
+
+export const Form: FC<Props> = ({ addTask }) => {
+  const [name, setName] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addTask(name);
+    setName("");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
-      <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="タスク名を入力"
+          onChange={handleChange}
+        />
+        <button type="submit">新規作成</button>
+      </form>
     </div>
   );
 };
